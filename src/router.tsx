5825c95d783c1bb56fe32499115bdb3d130970e5
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-04 23:36:23 +0000

    Create Router Configuration

---
+++ src/router.tsx
@@ -0,0 +1,32 @@
+
+import { createBrowserRouter } from 'react-router-dom'
+import SaasLayout from './components/SaasLayout'
+import Pipeline from './pages/Pipeline'
+import Pricing from './pages/Pricing'
+import ProfilePage from './pages/ProfilePage'
+import Settings from './pages/Settings'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <SaasLayout />,
+    children: [
+      {
+        index: true,
+        element: <Pipeline />,
+      },
+      {
+        path: 'pricing',
+        element: <Pricing />,
+      },
+      {
+        path: 'profile',
+        element: <ProfilePage />,
+      },
+      {
+        path: 'settings',
+        element: <Settings />,
+      },
+    ],
+  },
+])
