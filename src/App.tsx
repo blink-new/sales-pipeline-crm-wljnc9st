0c52a1fb168977f9862ec9b1a23208ea8c2a99d6
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-04 23:10:47 +0000

    Add Profile Page Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import { SaasLayout } from './components/SaasLayout'
-import { Pipeline } from './pages/Pipeline'
-import { Settings } from './pages/Settings'
-import { Toaster } from './components/ui/toaster'
-import { TooltipProvider } from './components/ui/tooltip'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ProfilePage } from './pages/ProfilePage'
 
-export default function App() {
+function App() {
   return (
-    <TooltipProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<SaasLayout />}>
-            <Route index element={<Navigate to="/pipeline" replace />} />
-            <Route path="pipeline" element={<Pipeline />} />
-            <Route path="settings" element={<Settings />} />
-          </Route>
-        </Routes>
-        <Toaster />
-      </Router>
-    </TooltipProvider>
+    <Router>
+      <Routes>
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </Router>
   )
 }
+
+export default App
