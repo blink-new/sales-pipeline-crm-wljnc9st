77712b64cff466d53f89496f3976fec571b03256
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-04 23:11:29 +0000

    Restore All Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Pipeline } from './pages/Pipeline'
+import { Settings } from './pages/Settings'
 import { ProfilePage } from './pages/ProfilePage'
-import { Navbar } from './components/Navbar'
+import { SaasLayout } from './components/SaasLayout'
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-background">
-        <Navbar />
+      <SaasLayout>
         <Routes>
+          <Route path="/" element={<Pipeline />} />
+          <Route path="/pipeline" element={<Pipeline />} />
+          <Route path="/settings" element={<Settings />} />
           <Route path="/profile" element={<ProfilePage />} />
         </Routes>
-      </div>
+      </SaasLayout>
     </Router>
   )
 }
